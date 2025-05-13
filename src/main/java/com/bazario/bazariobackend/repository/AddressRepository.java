51f868a490b483373cd4fac9a1e761e58f70c241
Nikhil Singh <thakurnikhilsingh2004@gmail.com>
2025-05-14 03:25:45 +0530

    Create CartItem Repository

--- src/main/java/com/bazario/bazariobackend/repository/AddressRepository.java
+++ src/main/java/com/bazario/bazariobackend/repository/AddressRepository.java
@@ -2,6 +2,8 @@ package com.bazario.bazariobackend.repository;
 
 import com.bazario.bazariobackend.entity.Address;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Long> {
 }
