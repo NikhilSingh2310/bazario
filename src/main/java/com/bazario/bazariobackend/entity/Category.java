a970bbece26fe63b0145b1ee72771598647c0848
Nikhil Singh <thakurnikhilsingh2004@gmail.com>
2025-05-14 01:00:15 +0530

    Update Category Entity with MySql Integration

--- src/main/java/com/bazario/bazariobackend/entity/Category.java
+++ src/main/java/com/bazario/bazariobackend/entity/Category.java
@@ -1,36 +1,28 @@
 package com.bazario.bazariobackend.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Entity class representing a product category in the system.
- * Categories are used to organize and classify products.
- *
- * @author Bazario Team
- * @version 1.0
- */
-@Entity
-@Table(name = "categories")
+import java.util.List;
+
+@Entity(name = "categories")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Category {
-    /**
-     * Unique identifier for the category.
-     */
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "category_id")
     private Long categoryId;
-
-    /**
-     * Name of the category.
-     * This field cannot be null and must not be empty.
-     */
-    @Column(name = "category_name", nullable = false)
+    @NotBlank
+    @Size(min = 5, message = "Category name must contain atleast 5 characters")
     private String categoryName;
 
+
 }
+
