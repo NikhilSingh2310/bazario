fe625d3b245084c9e21535e9f09871bf1990c14b
Nikhil Singh <thakurnikhilsingh2004@gmail.com>
2025-05-14 01:19:37 +0530

    Update Product

--- src/main/java/com/bazario/bazariobackend/entity/Product.java
+++ src/main/java/com/bazario/bazariobackend/entity/Product.java
@@ -41,4 +41,12 @@ public class Product {
     @JoinColumn(name = "category_id")
     private Category category;
 
+    @ManyToOne
+    @JoinColumn(name = "seller_id")
+    private User user;
+
+    @OneToMany(mappedBy = "product", cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    private List<CartItem> products = new ArrayList<>();
+
+
 }
